d5513bbcc65bb1acf32a9238042cf9a478bcf14a
aangeles2004 <78290413+aangeles2004@users.noreply.github.com>
2021-01-30 23:45:07 -0600

    Add files via upload
    01/30

---
+++ audrianaangelescommentsOutline.py
@@ -0,0 +1,20 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+#fdndjfkdbfkdhbfkhdbfghdfbghdfbgj
+
+
+#2) Make a multi-line comment.
+'''
+eirhhrhfkdbfkhdsfhdbfhsbdfhsd
+fdjgnkfgfjg
+dkjfdfbdh
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+r = 5 #idkifIadoingthisright
