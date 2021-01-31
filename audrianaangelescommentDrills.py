51340dea47391db4f7124c28db789021d6289efc
aangeles2004 <78290413+aangeles2004@users.noreply.github.com>
2021-01-30 23:52:14 -0600

    Add files via upload
    01/30

---
+++ audrianaangelescommentDrills.py
@@ -0,0 +1,43 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+#fjngfdjgfdbgdfgbkdfgb
+
+
+
+#2) Make a multi-line comment.
+'''
+dhfdhfgsdhfhad
+fhsdgfhdgfhd
+fbfhdfh
+'''
+
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+m= hi #sayhello
+
+#4) Make a single line comment.
+#kdngfgnfgbnfdgfgn
+
+
+#5) Make a multi-line comment.
+'''
+fjghhkhjcxhvcxkvhbcxbhv
+'''
+
