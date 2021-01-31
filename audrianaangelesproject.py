31b83c703afbe0033a7e250096ee42698ac4e471
aangeles2004 <78290413+aangeles2004@users.noreply.github.com>
2021-01-31 12:25:07 -0600

    Add files via upload
    01/31

---
+++ audrianaangelesproject.py
@@ -0,0 +1,33 @@
+
+#This project will ask you questions and and print our your response and I am the author
+audriana angeles
+'''
+Ask the user five different questions and get their answers
+The questions are:
+What is your name?
+What is your favorite color?
+What city did you grow up in?
+What is your best friend’s name?
+What color hair do you have?
+Store the user’s answers in variables
+Print the user’s answers.
+'''
+text_to_user = "what is your name?"
+name = input(text_to_user)
+print(name)
+
+text_to_user = "what is your favorite color?"
+favColor = input(text_to_user)
+print(favColor)
+
+text_to_user = "what city did you grow up in?"
+city = input(text_to_user)
+print(city)
+
+text_to_user = "what is your bestfriend's name?"
+Bff = input(text_to_user)
+print(Bff)
+
+text_to_user = "what is your hair color?"
+hair = input(text_to_user)
+print(hair)
