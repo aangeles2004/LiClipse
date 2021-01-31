dd7ea4e4a8289af837a7771987d445e4c87e42db
aangeles2004 <78290413+aangeles2004@users.noreply.github.com>
2021-01-31 00:39:24 -0600

    Add files via upload
    01/31

---
+++ audrianaangelesinputDrills.py
@@ -0,0 +1,53 @@
+'''
+For this assignment you should read the task, then below the task do what it 
+asks you to do.
+
+EXAMPLE TASK:
+'''
+#EX) Get input from the user to the question "What is your favorite color?"
+#and store their answer in a variable called favColor.
+favColor = input("What is your favorite color?")
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Get input from the user to the question "What is your favorite animal?"
+#and store their answer in a variable called favAnimal.
+text_to_user = "what is your fav animal?"
+favAnimal = input(text_to_user)
+
+
+
+#2) Get input from the user to the question "What is your favorite number?"
+#and store their answer in a variable called favNum. Convert favNum from a 
+#string variable to a int variable.
+
+text_to_user = "what is your fav number?"
+favNum = input(text_to_user)
+favNum = 16
+
+#3) Get input from the user to the question "What is your school's name?"
+#and store their answer in a variable called school.
+text_to_user = "what is your schools name?"
+school = input(text_to_user)
+
+
+#4) Get input from the user to the question "What is your instructors name?"
+#and store their answer in a variable called instructor.
+text_to_user = "what is your instructors name?"
+instructor = input(text_to_user)
+
+
+
+#5) Get input from the user to the question "What is 2 + 2?"
+#and store their answer in a variable called ans. Convert ans from a string
+#variable to a int variable
+text_to_user = "what is  2 + 2 ?"
+ans = input(text_to_user)
+ans = 16
+
