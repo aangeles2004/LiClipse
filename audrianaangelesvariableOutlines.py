b3a7fb270ad439c5f2f661a7f7d4fd3d6c968236
aangeles2004 <78290413+aangeles2004@users.noreply.github.com>
2021-04-22 17:19:07 -0500

    Add files via upload

---
+++ audrianaangelesvariableOutlines.py
@@ -0,0 +1,94 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+
+A = 9 
+A = 3.45
+A = “youwin”;
+A = true 
+A = [ 2 , 4 ,6]
+
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+vara  = 4 varA= 2
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+A = hi
+A = bye 
+print
+
+#4)
+#Make a variable and set it to a variable you have already created:
+Var = 16
+Lista = 10
+Var = “lista”
+
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+
+B = [ c , dc , we]
+Listvar = list[2]
+print[listvar]
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first
+#character in that string:
+
+String = “micheal” 
+print(string[0])
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+print (string[a])
+
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+
+A = 
+
+print(sumTwoNum)
+#6) Define a function that has three int parameters. If the first number is 
+#equal to the second OR the third number, return true. Else, return false. Now 
+#call the function.
+#Print what the function returns.
+def true_false(1,2,3):
+if (1=2)or (1=3)
+return True
+else
+return False
+print(return)
+#7) Define a function that prints your name. It should have no parameters and 
+#shouldn't return anything. Now call the function.
+
+def Audriana
+ print(def):
+
+#8) Define a function that has one string parameter. The string should be a
+#color. If that string is equal to your favorite color, it prints "That's my 
+#favorite color!". If it is not, it prints "That is not my favorite color. 
+#Try again.". It shouldn't return anything. Now call the function.
+def string_colors("blue"):
+    "blue" = Favorite color
+    print("Thats my favorite color!")
+
+
+#9) Define a function that has one int parameter. The int should be 
+#positive. If the number is not equal to zero, the function runs a loop that
+#decrements the parameter by 1 and prints it each time. Now call the function.
+
+defint_positive(100):
+while(100 == 0)
+print(100)
+100 == 100 -1 
