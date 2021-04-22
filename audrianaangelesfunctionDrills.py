ab2431478a487d0ee4301e5dbfb0ec39783d22f5
aangeles2004 <78290413+aangeles2004@users.noreply.github.com>
2021-04-22 17:09:20 -0500

    Add files via upload

---
+++ audrianaangelesfunctionDrills.py
@@ -0,0 +1,105 @@
+'''
+For this assignment you should read the task, then below the task do what it asks you to do.
+For tasks with return statements, you can test out if you are correct by putting in some parameters
+and printing what is returned outside of the function.
+
+EXAMPLE TASK:
+'''
+#EX) Define a function that has two parameters. Make the function add the two
+#numbers together and return the result.
+def add_two_numbers(num1, num2):
+    sumOfTwoNumbers = num1 + num2
+    return sumOfTwoNumbers
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+
+#1) Define a function that has two int parameters. Make the function subtract 
+#the first parameter minus the second one. Then, return the result. Now call 
+#the function.
+#Print what the function returns.
+def twoint(int1 , int2):
+    subtracat = int1 - int2
+    return substract
+
+x = 2
+y = 1
+
+int = twoint(x,y)
+print(int)
+
+
+#2) Define a function that has one parameter. Make the function divide the 
+#parameter by 2, multiply it by 77, and then add 10,000. Return the result.
+#Now call the function.
+#Print what the function returns.
+
+def operations(12):
+    QuotProdSym = 12/2 * 77 = 1000
+    return QuotProdSym
+print(QuotProdSym)
+
+#3) Define a function that has two int parameters. Make the function check if 
+#two numbers are equal. If they are equal, return true. If they are not equal, 
+#return false. Now call the function.
+#Print what the function returns.
+def truth(1,1):
+twolnt = 1 == 1
+return True
+print(True)
+#4) Define a function that has two int parameters. Make the function
+#check which parameter is bigger, and return the bigger parameter. 
+#If they are the same, it should just return either parameter. Now call the 
+#function.
+#Print what the function returns.
+def bigger_num(1,2):
+twolntNum = if (2 >1) or (1>2):
+biggst =2
+return biggest
+print(biggest)
+#5) Define a function that has two string parameters. Make the function
+#add the two strings together. And then return the combined string. Now call 
+#the function.
+def two_strings("ally", "all2"):#Print what the function returns.
+sumTwoNum = "ally" + "ally2"
+return sumTwoNum
+print(sumTwoNum)
+#6) Define a function that has three int parameters. If the first number is 
+#equal to the second OR the third number, return true. Else, return false. Now 
+#call the function.
+#Print what the function returns.
+def true_false(1,2,3):
+if (1=2)or (1=3)
+return True
+else
+return False
+print(return)
+#7) Define a function that prints your name. It should have no parameters and 
+#shouldn't return anything. Now call the function.
+
+def Audriana
+ print(def):
+
+#8) Define a function that has one string parameter. The string should be a
+#color. If that string is equal to your favorite color, it prints "That's my 
+#favorite color!". If it is not, it prints "That is not my favorite color. 
+#Try again.". It shouldn't return anything. Now call the function.
+def string_colors("blue"):
+    "blue" = Favorite color
+    print("Thats my favorite color!")
+
+
+#9) Define a function that has one int parameter. The int should be 
+#positive. If the number is not equal to zero, the function runs a loop that
+#decrements the parameter by 1 and prints it each time. Now call the function.
+
+defint_positive(100):
+while(100 == 0)
+print(100)
+100 == 100 -1 
---
+++ audrianaangelesfunctionOutline.py
@@ -0,0 +1,38 @@
+'''
+This outline will help solidify concepts from the Functions lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a function that has two boolean parameters. If both booleans are
+#true, return true. Else, return false. Then, call the function.
+#Print what the function returns.
+
+def truetofalse(t,c):
+    if(t + c): True 
+    else: False 
+    print(truetofalse)
+    return truetofalse
+ 
+
+#2) Make a function that takes one int parameter: gallons. Convert gallons
+#to cups (do this by multiplying gallons by 16). Then return cups. Then,
+#call the function.
+#Print what the function returns.
+
+defgallonstocups(gallons):
+cups = gallons * 16
+return cups 
+
+gallonstocups(16)
+print(gsllondtocups)
+
+#3) Make a function of any any kind with a common SYNTAX error. Then, call the
+#function.
+#Print what the function returns.
+defgallonstocups(gallons
+cups = gallons * 16
+return cups 
+
+gallonstocups(16)
+print(gsllondtocups)
+
